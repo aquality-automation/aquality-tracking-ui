df573a726066de8e6342cf288725c9f2a108fa58
VladKostyukevich <vlad.kostyukevich@gmail.com>
2020-01-04 10:43:59 +0300

    Fix styles

--- e2e/specs/api/publicApi.spec.ts
+++ e2e/specs/api/publicApi.spec.ts
@@ -20,7 +20,6 @@ describe('Public API tests', () => {
     await projectHelper.importer.executeCucumberImport(suite.name, [cucumberImport], [`${builds.build_1}.json`]);
     suite.project_id = projectHelper.project.id;
     suite = (await projectHelper.editorAPI.getSuites(suite))[0];
-    return;
   });
 
   afterAll(async () => {
@@ -34,16 +33,20 @@ describe('Public API tests', () => {
       project_id: projectHelper.project.id,
       test_suite_id: suite.id
     });
-    expect(testrun.id).toBeDefined('Test run was not created');
+    return expect(testrun.id).toBeDefined('Test run was not created');
   });
 
   it('Tests can be found for Suite', async () => {
     tests = await projectHelper.editorAPI.getTests({ test_suite_id: suite.id, project_id: projectHelper.project.id });
-    expect(tests.length).toBe(3, 'Tests number is not correct');
+    return expect(tests.length).toBe(3, 'Tests number is not correct');
   });
 
   it('Test Result can be found by test tun and test id', async () => {
-    const testResults = await projectHelper.editorAPI.getResults({ test_id: tests[0].id, test_run_id: testrun.id, project_id: projectHelper.project.id });
+    const testResults = await projectHelper.editorAPI.getResults({
+      test_id: tests[0].id,
+      test_run_id: testrun.id,
+      project_id: projectHelper.project.id
+    });
     expect(testResults.length).toBe(1, 'Test Results number is not correct');
     expect(testResults[0].final_result_id).toBe(3, 'Final Result should be Not Executed!');
     testresult = testResults[0];
@@ -52,14 +55,14 @@ describe('Public API tests', () => {
   it('Test Result can be updated', async () => {
     testresult.final_result_id = 4;
     testresult = await projectHelper.editorAPI.createResult(testresult);
-    expect(testresult.final_result_id).toBe(4, 'Final Result should be In Progress!');
+    return expect(testresult.final_result_id).toBe(4, 'Final Result should be In Progress!');
   });
 
   it('Test can be updated', async () => {
     let test: Test = tests[0];
     test.body = 'New Test Body';
     test = await projectHelper.editorAPI.createTest(test);
-    expect(test.body).toBe('New Test Body', 'Test Body Should be updated!');
+    return expect(test.body).toBe('New Test Body', 'Test Body Should be updated!');
   });
 
   it('Test can be created', async () => {
@@ -69,11 +72,11 @@ describe('Public API tests', () => {
         project_id: projectHelper.project.id,
         suites: [{ id: suite.id, project_id: projectHelper.project.id }]
       });
-    expect(newtest.id).toBeDefined('Test was not created');
+      return expect(newtest.id).toBeDefined('Test was not created');
   });
 
   it('Test Result can be created', async () => {
-    const newResult = await await projectHelper.editorAPI.createResult(
+    const newResult = await projectHelper.editorAPI.createResult(
       {
         project_id: projectHelper.project.id,
         test_id: newtest.id,
@@ -81,6 +84,6 @@ describe('Public API tests', () => {
         test_run_id: testrun.id,
         start_date: new Date()
       });
-    expect(newResult.id).toBeDefined('Test Result was not created');
+      return expect(newResult.id).toBeDefined('Test Result was not created');
   });
 });
