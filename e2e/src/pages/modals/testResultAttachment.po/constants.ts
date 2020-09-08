f2d7fc82180b0a674c5cf752420353d8e7eac6f7
d.bogatko <d.bogatko@a1qa.com>
2020-09-08 13:17:50 +0300

    changed locator for sub-titile cause of changes in html

--- e2e/src/pages/modals/testResultAttachment.po/constants.ts
+++ e2e/src/pages/modals/testResultAttachment.po/constants.ts
@@ -10,7 +10,7 @@ export const fileButton = (name: string) =>
 export const elements = {
     uniqueElement: element(by.tagName('attachment-modal')),
     titleElement: element(by.xpath('//attachment-modal//h4[contains(@class,"modal-title")]')),
-    subTitleElement: element(by.xpath('//attachment-modal//h4[contains(@class,"modal-sub-title")]')),
+    subTitleElement: element(by.xpath('//attachment-modal//h6[contains(@class,"modal-sub-title")]')),
     downloadElement: element(by.xpath('//attachment-modal//button[contains(@class,"btn-success")]')),
     closeElement: element(by.xpath('//attachment-modal//button[contains(@class,"btn-secondary")]')),
     frameElement: element(by.xpath('//attachment-modal//iframe')),
